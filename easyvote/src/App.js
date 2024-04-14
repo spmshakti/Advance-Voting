e9d5f741e0b84a0e4d103de711d5d9d9e18de1e2
Prakash Singh <prakashsingh8738@gmail.com>
2024-04-14 11:38:23 +0530

    Landing page

--- easyvote/src/App.js
+++ easyvote/src/App.js
@@ -1,11 +1,18 @@
+import LandingPage from "./component/LandingPage";
 import Login from "./component/Login";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import News2 from "./component/News.2";
 
 
 
 function App() {
   return (
     <div className="App">
-      <Login />
+    
+      <LandingPage />
+        
+      
     </div>
   );
 }
--- easyvote/src/component/LandingPage.js
+++ easyvote/src/component/LandingPage.js
@@ -1,11 +1,63 @@
-import React from 'react'
+import React from 'react';
+import News2 from './News.2';
+import Login from './Login';
 
-function LandingPage() {
+const LandingPage = () => {
   return (
-    <div>
-      
+    <div className="bg-[#9FE5F4] min-h-screen">
+      <nav className="p-6 bg-white flex justify-between">
+        <h1 className="font-bold text-4xl text-blue-700">EasyVote</h1>
+        <div className='font-bold'>
+          <a href="#" className="mx-2">Contact Us</a>
+          <a href="#" className="mx-2">About Us</a>
+          <a href="#" className="mx-2">Register</a>
+          <button className="mx-2 bg-blue-500 text-white px-4 py-2 rounded">Login</button>
+        </div>
+      </nav>
+      <main className="p-6">
+        <div className='flex justify-between'>
+          <div className='w-[300px] h-[100px]'>
+            <p className="font-bold  text-4xl mb-6">Secure Voting Made <span className='text-blue-700'>Simple</span></p>
+            <p className="mb-2 font-bold ">Easy Vote will help you to vote online for your constituency</p>
+          </div>
+          <img src="/familypic.png" className='w-[180px] h-[180px]' alt="pic" />
+        </div>
+        <div className='mx-auto w-fit items-center'>
+          <p className="my-10 font-bold text-3xl text-black mb-12 px-6">Your Vote Is Valuable</p>
+          <div className=''>
+            <button className="bg-white flex items-center justify-center text-black font-medium rounded-md px-1 py-2 my-3">
+                  <img
+                    src="/voteIcon.svg"
+                    className="w-[50px] h-[50px] pl-2"
+                    alt="Google Icon"
+                  />
+                  <p className='w-[300px] font-bold'>Vote Here</p>
+              </button><br></br>
+              <button className="bg-white flex items-center justify-center text-black font-medium rounded-md px-1 py-2 my-3">
+                  <img
+                    src="/partiesIcon.svg"
+                    className="w-[50px] h-[50px] pl-2"
+                    alt="Google Icon"
+                  />
+                  <p className='w-[300px] font-bold'>Visit Party</p>
+              </button><br></br>
+              <button className="bg-white flex items-center justify-center text-black font-medium rounded-md px-1 py-2 my-3">
+                  <img
+                    src="/searchIcon.svg"
+                    className="w-[50px] h-[50px] pl-2"
+                    alt="Google Icon"
+                  />
+                  <p className='w-[300px] font-bold'>Search For Candidate</p>
+              </button><br></br>
+          </div>
+        </div>
+        
+      </main>
+      <div>
+        <News2 />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default LandingPage
+export default LandingPage;
