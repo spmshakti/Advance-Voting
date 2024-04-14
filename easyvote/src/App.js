d8333f877acacd916b8f29fae8940259fcca0aae
Prakash Singh <prakashsingh8738@gmail.com>
2024-04-14 12:05:21 +0530

    commit

--- easyvote/src/App.js
+++ easyvote/src/App.js
@@ -3,9 +3,6 @@ import Login from "./component/Login";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import News2 from "./component/News.2";
-
-
-
 function App() {
   return (
     <div className="App">
